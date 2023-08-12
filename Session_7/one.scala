92e6a329945fe2467e17664b5f766fa136188440
gprabhath1 <geethikaprabhath1@gmail.com>
2023-08-12 21:47:58 +0530

    session 7 edit?

--- Session_7/one.scala
+++ Session_7/one.scala
@@ -1,7 +1,5 @@
 object one{
-  def filterEvenNumbers(numbers: List[Int]): List[Int] = {
-    numbers.filter(num => num % 2 == 0)
-  }
+  def filterEvenNumbers(numbers: List[Int]): List[Int] = numbers.filter(num => num % 2 == 0)
 
   def main(args: Array[String]): Unit = {
     val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
--- Session_7/two.scala
+++ Session_7/two.scala
@@ -1,7 +1,5 @@
 object two{
-  def calculateSquare(numbers: List[Int]): List[Int] = {
-    numbers.map(num => num * num)
-  }
+  def calculateSquare(numbers: List[Int]): List[Int] = numbers.map(num => num * num)
 
   def main(args: Array[String]): Unit = {
     val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
